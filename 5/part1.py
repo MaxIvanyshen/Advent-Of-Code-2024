b2b57d32ebc8a8e3b4dd0786ea3bde759e8e9716
MaxIvanyshen <maxivanyshen@gmail.com>
2024-12-05 11:11:40 +0200

    part 1 of day 5 is solved

---
+++ 5/part1.py
@@ -0,0 +1,36 @@
+
+arr = []
+endOfRules = -1
+with open('./input') as file:
+    for (i, line) in enumerate(file):
+        line = line.strip()
+        
+        if line == '':
+            endOfRules = i
+        arr.append(line)
+
+rules = arr[:endOfRules]
+orders = arr[endOfRules+1:]
+
+map = {}
+
+for rule in rules:
+    before, after = rule.split("|")
+    if int(after) not in map:
+        map[int(after)] = set()
+
+    map[int(after)].add(int(before))
+
+ans = 0
+for order in orders:
+    nums = [int(c) for c in order.split(",")]
+    valid = True
+    for i in range(len(nums)):
+        for j in range(0, i):
+            if nums[j] in map and nums[i] in map[nums[]]:
+                valid = False
+    if valid:
+        ans += nums[len(nums) // 2]
+
+print(ans)
+
