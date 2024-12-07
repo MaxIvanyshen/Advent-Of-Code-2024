779b26074f06aae7db7dba6ba170c25e22267115
MaxIvanyshen <maxivanyshen@gmail.com>
2024-12-07 11:39:41 +0200

    solve part 1 of day 7

---
+++ 7/part1.py
@@ -0,0 +1,25 @@
+
+def possible(result, values, after_idx, curr=0):
+    if after_idx >= len(values):
+        if curr == result:
+            return True
+        else:
+            return False
+
+    plus = possible(result, values, after_idx + 1, curr + values[after_idx])
+    mult = possible(result, values, after_idx + 1, curr * values[after_idx] if curr != 0 else values[after_idx])
+
+    return plus or mult
+
+
+ans = 0
+with open("./input") as file:
+    for line in file:
+        result, values = line.split(":")
+        result = int(result)
+        values = list(int(c) for c in values.strip().split())
+
+        if possible(result, values, 0):
+            ans += result
+
+print(ans)
