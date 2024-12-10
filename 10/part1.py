05502b87a0c95f262d39fc5516867cf1173e036a
MaxIvanyshen <maxivanyshen@gmail.com>
2024-12-10 14:06:18 +0200

    solve part 1 of day 10

---
+++ 10/part1.py
@@ -0,0 +1,48 @@
+
+board = []
+with open("./input") as file:
+    for line in file:
+        board.append([int(c) for c in list(line.strip())])
+
+starts = []
+ROWS, COLS = len(board), len(board[0])
+for r in range(ROWS):
+    for c in range(COLS):
+        if board[r][c] == 0:
+            starts.append((r, c))
+
+def get_score(board, pos, found, curr=0):
+    r, c = pos
+    if not (
+        r in range(len(board))
+        and c in range(len(board[r]))
+    ):
+        return 0
+    if board[r][c] != curr:
+        return 0
+    if curr == 9 == board[r][c]:
+        if pos in found:
+            return 0
+        found.add(pos)
+        return 1
+
+    ans = 0
+    dirs = [
+        [0, 1],
+        [0, -1],
+        [1, 0],
+        [-1, 0],
+    ]
+    for dir in dirs:
+        next_pos = (r + dir[0], c + dir[1])
+        ans += get_score(board, next_pos, found, curr + 1)
+    return ans
+
+ans = 0
+for s in starts:
+    found = set()
+    curr = get_score(board, s, found, 0)
+    ans += curr
+
+print(ans)
+
